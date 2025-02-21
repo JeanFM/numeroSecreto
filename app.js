0cff0fcd789e1962b74ff37b763ff2473ce7765f
JeanFM <jeanfrancis75@gmail.com>
2025-02-21 17:10:06 -0300

    alterando o limite para 100

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
-    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1});
 }
 
 function exibirMensagemInicial(){
     exibirTextoNaTela('h1', 'Jogo de número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 3');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 }
 
 exibirMensagemInicial();
